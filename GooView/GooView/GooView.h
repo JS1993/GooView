a4b1a2440d5d9f01e2fe626234eab9f51337b37b
susu <28025920@qq.com>
2016-04-25 21:22:46 +0800

    完成QQ消息数量提醒，拖拽消失的效果

---
+++ GooView/GooView/GooView.h
@@ -0,0 +1,13 @@
+//
+//  GooView.h
+//  GooView
+//
+//  Created by  江苏 on 16/4/25.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GooView : UIButton
+
+@end
---
+++ GooView/GooView/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  GooView
+//
+//  Created by  江苏 on 16/4/25.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.translatesAutoresizingMaskIntoConstraints=NO;
+    
+}
+
+
+@end
